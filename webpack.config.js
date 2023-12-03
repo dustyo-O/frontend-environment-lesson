d03ef064c310c68be8d8d899cc7adfb15dc1a02f
Alexandr Shleiko <dusty@Alexandrs-MacBook-Pro.local>
2023-12-03 17:42:06 +0100

    webpack basic config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+};
